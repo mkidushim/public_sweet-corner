c3869b00ae911ae43081365c3afed9855249338e
mkidushim <mkidushim@gmail.com>
2015-05-28 08:36:24 -0700

    added different names to files to make ajax call easier to read

--- sweet.js
+++ sweet.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('body').on('click', '.contact', function() {
         $.ajax({
-            url: 'template_test.html',
+            url: 'template_contact.html',
             dataType: 'html',
             cache: false,
             success: function(response) {
@@ -10,9 +10,20 @@ $(document).ready(function(){
             }
         })
     })
-        $('body').on('click', '.home', function() {
+    $('body').on('touchstart', '.contact', function() {
         $.ajax({
-            url: 'template_test2.html',
+            url: 'template_contact.html',
+            dataType: 'html',
+            cache: false,
+            success: function(response) {
+                console.log('success:', response)
+                $('#main_content').html(response)
+            }
+        })
+    });
+    $('body').on('click', '.home', function() {
+        $.ajax({
+            url: 'template_home.html',
             dataType: 'html',
             cache: false,
             success: function(response) {
@@ -21,6 +32,38 @@ $(document).ready(function(){
             }
         })
     })
+    $('body').on('touchstart', '.home', function() {
+        $.ajax({
+            url: 'template_home.html',
+            dataType: 'html',
+            cache: false,
+            success: function(response) {
+                console.log('success:', response)
+                $('#main_content').html(response)
+            }
+        })
+    });
+    $('body').on('click', '.services', function() {
+        $.ajax({
+            url: 'template_services.html',
+            dataType: 'html',
+            cache: false,
+            success: function(response) {
+                console.log('success:', response)
+                $('#main_content').html(response)
+            }
+        })
+    })
+    $('body').on('touchstart', '.services', function() {
+        $.ajax({
+            url: 'template_services.html',
+            dataType: 'html',
+            cache: false,
+            success: function(response) {
+                console.log('success:', response)
+                $('#main_content').html(response)
+            }
+        })
+    });
 
 })
-
