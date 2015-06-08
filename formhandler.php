4fb651abf036501d179795477624d3eb86c8de83
mkidushim <mkidushim@gmail.com>
2015-06-08 10:07:12 -0700

    working on 3.0

--- formhandler.php
+++ formhandler.php
@@ -5,7 +5,29 @@ $email = $_GET["email"];
 $phone = $_GET["phone"];
 $subject = $_GET["subject"];
 $message = $_GET["message"];
-$array = ['name' => $name, 'email'=> $email, 'phone' => $phone, 'subject' => $subject, 'message' => $message];
+//$array = ['name' => $name, 'email'=> $email, 'phone' => $phone, 'subject' => $subject, 'message' => $message];
 
-print_r($array);
- ?>
+//print_r($array);
+foreach($_post as $key => $value){
+	switch ($key) {
+		case 'first_name':
+			if(preg_match(^[A-Z][a-zA-Z]+$, $_post['first_name']))
+			break;
+		case 'last_name':
+			if(preg_match(pattern, subject))
+			break;
+		case 'first_name':
+			if(preg_match(pattern, subject))
+			break;
+		case 'first_name':
+			if(preg_match(pattern, subject))
+			break;
+		case 'first_name':
+			if(preg_match(pattern, subject))
+			break;
+		default:
+			# code...
+			break;
+	}
+}
+ ?>}
